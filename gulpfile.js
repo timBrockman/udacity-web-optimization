a1d3cd25e0b5e28da935de6117fb4a13def318e7
timBrockman <tim@timbrockman.com>
2015-10-06 15:30:39 -0400

    add gulpfile.js and node modules

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+//var ngrok = require('ngrok');
+
+var gulp  = require('gulp');
+//var gOpen = require('gulp-open');
+//var shell = require('gulp-shell');
+var browserSync = require('browser-sync').create();
+
+// Static Server is fine
+gulp.task('serve', function(){
+
+  browserSync.init({
+      server: {
+            baseDir:"./"}
+      });
+  gulp.watch(['*.html'], browserSync.reload);
+  gulp.watch(['css/*.css'], browserSync.reload);
+  gulp.watch(['scripts/**/*.js'], browserSync.reload);
+  gulp.watch(['images/**/*'], browserSync.reload);
+  });
+/* this is failing
+gulp.task('ngrok', function(){
+  browserSync.init({server:{baseDir:"./"}},
+    function(err, bs){
+      ngrok.connect({proto:'http',addr:3000}, function(err,url){
+      //  console.log(url);
+        gulp.src('')
+        .pipe(gOpen({app:'chrome',uri:'http://localhost:4040'}));
+      });
+    });
+  //shell.task('c:/ngrok/ngrok http 3000', {'verbose':true});
+
+});
+*/
+
+gulp.task('default',['serve']);
+/*gulp.task('reset', function(){
+   browserSync.exit()
+   console.log('Dev server shut down.')
+   browserSync.reload()
+ });*/
