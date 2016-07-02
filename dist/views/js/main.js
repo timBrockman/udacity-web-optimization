56049e9c7215a75cb725052f82fa6efa599c5ca6
timBrockman <tim@timbrockman.com>
2016-07-02 19:17:36 -0400

    limit # of pizzas

--- dist/views/js/main.js
+++ dist/views/js/main.js
@@ -537,18 +537,20 @@ function scrollHandler(){
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
+  var ylim = Math.floor(window.innerHeight/256);
+  var lim = Math.floor(ylim/cols);
   setPizzaSizes();
   for (var i = 0; i < 24; i++) {
-    var elem = document.createElement('img');
+    elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
+    document.getElementById("#movingPizzas1").appendChild(elem);
   }
   updatePositions();
-  movers = document.querySelectorAll('.mover');
+  movers = document.getElementsByClassName('.mover');
 
 });
